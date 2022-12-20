fcaaf3599da3bc77525762def90cb6eca145152d
Bruno López González <bruno.lopezgonzalez@WIRTZ.LOCAL>
2022-12-20 18:26:39 +0100

    Reestructuración de carpetas y proyecto inicial

---
+++ yourfolio/src/main/java/io/yourfol/yourfolio/YourfolioApplication.java
@@ -0,0 +1,13 @@
+package io.yourfol.yourfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourfolioApplication.class, args);
+	}
+
+}
---
+++ yourfolio/src/test/java/io/yourfol/yourfolio/YourfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yourfol.yourfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
