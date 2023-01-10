7e10519b0b3099797e4550fc7d15823e7ab8ded9
Bruno López González <bruno.lopezgonzalez@WIRTZ.LOCAL>
2023-01-10 19:23:05 +0100

    Variables de estilo y de tab activa añadidas

--- yourfolio-frontend/js/head.js
+++ yourfolio-frontend/js/head.js
@@ -6,5 +6,6 @@ document.write(`
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 `);
