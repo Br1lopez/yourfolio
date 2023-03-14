843ec0552bc749ae0a53232b1255c9bb5e16d89c
Bruno López <bruno.lpgz@gmail.com>
2023-03-14 17:35:48 +0100

    db changes

--- yourfolio-db/ddl.sql
+++ yourfolio-db/ddl.sql
@@ -22,7 +22,7 @@ CREATE TABLE file (
 
 CREATE TABLE portfolio (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    theme VARCHAR(255),
+    name VARCHAR(255),
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
@@ -31,7 +31,9 @@ CREATE TABLE project (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255),
+    thumbnail_id INT,
     user_id INT,
+    FOREIGN KEY (thumbnail_id) REFERENCES file(id),
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
@@ -47,6 +49,14 @@ CREATE TABLE section (
     name VARCHAR(255),
     tab_id INT,
     FOREIGN KEY (tab_id) REFERENCES tab(id)
+);
+
+CREATE TABLE style(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	bg_color VARCHAR(32),
+	font_color VARCHAR(32),
+	portfolio_id INT,
+   FOREIGN KEY (portfolio_id) REFERENCES portfolio(id)
 );
 
 CREATE TABLE file_project (
--- yourfolio-db/dml.sql
+++ yourfolio-db/dml.sql
@@ -4,13 +4,15 @@ INSERT INTO user (name, surname1, surname2, email, password) VALUES ("test@test.
 
 INSERT INTO file (url, description, user_id) VALUES ("/imgs/test.jpg", "Imagen de prueba", 1);
 
-INSERT INTO portfolio (theme, user_id) VALUES ("tema del portafolio", 1);
+INSERT INTO portfolio (name, user_id) VALUES ("tema del portafolio", 1);
 
-INSERT INTO project (name, description, user_id) VALUES ("Título del proyecto", "Descripción del proyecto", 1);
+INSERT INTO project (name, description, thumbnail_id, user_id) VALUES ("Título del proyecto", "Descripción del proyecto", 1, 1);
 
 INSERT INTO tab (template, portfolio_id) VALUES ("plantilla de pestaña", 1);
 
 INSERT INTO section (name, tab_id) VALUES ("Nombre de la sección", 1);
+
+INSERT INTO style(bg_color, font_color, portfolio_id) VALUES ("black", "white", 1);
 
 INSERT INTO file_project (file_id, project_id) VALUES (1, 1);
 
