6f945d923e987347dfdec8f13282c2ff87d84773
Bruno López <bruno.lpgz@gmail.com>
2023-03-16 11:13:14 +0100

    DML fix

--- yourfolio-db/dml.sql
+++ yourfolio-db/dml.sql
@@ -1,16 +1,16 @@
 use yourfolio;
 
-INSERT INTO user (name, surname1, surname2, email, password) VALUES ("test@test.com", "contraseña", "Juan", "Pérez", "Rodríguez");
+INSERT INTO user (name, surname1, surname2, email, password) VALUES ("Joaquín", "Jiménez", "Rodríguez", "test@test.com", "contraseña");
 
-INSERT INTO file (url, description, user_id) VALUES ("/imgs/test.jpg", "Imagen de prueba", 1);
+INSERT INTO file (url, description, user_id) VALUES ("/imgs/test.jpg", "Imagen de unos macarrones", 1);
 
-INSERT INTO portfolio (name, user_id) VALUES ("tema del portafolio", 1);
+INSERT INTO portfolio (name, user_id) VALUES ("Joaquín Jiménez - Cocinero", 1);
 
-INSERT INTO project (name, description, thumbnail_id) VALUES ("Título del proyecto", "Descripción del proyecto", 1);
+INSERT INTO project (name, description, thumbnail_id) VALUES ("Macarrones", "Macarrones con queso", 1);
 
-INSERT INTO tab (template, portfolio_id) VALUES ("plantilla de pestaña", 1);
+INSERT INTO tab (template, portfolio_id) VALUES ("Pasta italiana", 1);
 
-INSERT INTO section (name, tab_id) VALUES ("Nombre de la sección", 1);
+INSERT INTO section (name, tab_id) VALUES ("global", 1);
 
 INSERT INTO style(bg_color, font_color, portfolio_id) VALUES ("black", "white", 1);
 
