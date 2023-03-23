c69ae4fee1fd5b460d1cf4a9f60c3af64e254a6e
Bruno López <bruno.lpgz@gmail.com>
2023-03-23 12:51:42 +0100

    working

--- yourfolio-frontend-react/src/modules/pageCreator/components/navBar/NavBar.jsx
+++ yourfolio-frontend-react/src/modules/pageCreator/components/navBar/NavBar.jsx
@@ -3,31 +3,27 @@ import { Navbar, Button, Nav } from "react-bootstrap";
 import "./navBar.scss";
 import { NewTabModal } from "./NewTabModal";
 
-interface NavBarProps {
-  title: string;
-  sections?: string[];
-}
 
-export const NavBar = (props: NavBarProps) => {
-  const [show, setShow] = useState<boolean>(false);
+export const NavBar = ({title, sections}) => {
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
       <Navbar bg="light" expand="sm" id="navbar">
-        <Navbar.Brand href="index.html">{props.title}</Navbar.Brand>
+        <Navbar.Brand href="index.html">{title}</Navbar.Brand>
         <Navbar.Toggle aria-controls="navCollapse" />
         <Navbar.Collapse className="justify-content-end" id="navCollapse">
           <Nav>
-            {props.sections?.map((section) => {
+            {sections?.map((section) => {
               return <Nav.Link href="#">{section}</Nav.Link>;
             })}
             <Nav.Link href="#">
               <Button className="addTab" variant="link" onClick={handleShow}>
                 <i
                   className="fas fa-plus-circle"
-                  style={{ fontSize: "1.5em" }}
+                  style={{ fontSize: "1.5em", color: "red"  }}
                 ></i>
               </Button>
             </Nav.Link>
