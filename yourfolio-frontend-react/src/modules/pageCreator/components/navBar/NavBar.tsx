f115439cbb12bc76b6e073bd1b34f924d2b3ec80
Bruno López <bruno.lpgz@gmail.com>
2023-04-04 10:43:46 +0200

    wip

--- yourfolio-frontend-react/src/modules/pageCreator/components/navBar/NavBar.tsx
+++ yourfolio-frontend-react/src/modules/pageCreator/components/navBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Navbar, Button, Nav } from "react-bootstrap";
+import { Navbar, Button, Nav, Popover, OverlayTrigger } from "react-bootstrap";
 import "./navBar.scss";
 import { NewTabModal } from "./NewTabModal";
 
@@ -9,22 +9,85 @@ interface NavBarProps {
 }
 
 export const NavBar = (props: NavBarProps) => {
-  const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [showPopover, setShowPopover] = useState(false);
+
+  const handleContextMenu = (e: any) => {
+    setShowPopover(true);
+    e.preventDefault();
+  };
+
+  const handlePopoverClose = (e: any) => {
+    setShowPopover(false);
+    e.preventDefault();
+  };
 
   return (
     <>
       <Navbar expand="sm" id="navbar" className="navbar">
-        <Navbar.Brand className="navbar__brand" href="index.html">{props.title}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navCollapse" className="navbar__collapseButton"/>
+        <Navbar.Brand className="navbar__brand" href="index.html">
+          {props.title}
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="navCollapse"
+          className="navbar__collapseButton"
+        />
         <Navbar.Collapse className="justify-content-end" id="navCollapse">
           <Nav>
             {props.sections?.map((section) => {
-              return <Nav.Link className="navbar__tabLink" href="#">{section}</Nav.Link>;
+              return (
+                <Nav.Link
+                  className="navbar__tabLink"
+                  href="#"
+                  onContextMenu={handleContextMenu}
+                >
+                  {section}
+                  <OverlayTrigger
+                    show={showPopover}
+                    trigger="click"
+                    placement="right"
+                    overlay={
+                      <Popover>
+                        <Popover.Body>
+                          <div className="action-buttons-container">
+                            <span className="action-button">
+                              <i
+                                className="fas fa-edit"
+                                onClick={() => {
+                                  // Handle edit click here
+                                  setShowPopover(false);
+                                }}
+                              ></i>
+                            </span>
+                            <span className="action-button">
+                              <i
+                                className="fas fa-trash-alt"
+                                onClick={() => {
+                                  // Handle delete click here
+                                  setShowPopover(false);
+                                }}
+                              ></i>
+                            </span>
+                          </div>
+                        </Popover.Body>
+                      </Popover>
+                    }
+                    rootClose={true}
+                    // onHide={handlePopoverClose}
+                  >
+                    <Button variant="link"></Button>
+                  </OverlayTrigger>
+                </Nav.Link>
+              );
             })}
             <Nav.Link href="#">
-              <Button className="navbar__addTabButton" variant="link" onClick={handleShow}>
+              <Button
+                className="navbar__addTabButton"
+                variant="link"
+                onClick={handleShow}
+              >
                 <i className="fas fa-plus-circle"></i>
               </Button>
             </Nav.Link>
