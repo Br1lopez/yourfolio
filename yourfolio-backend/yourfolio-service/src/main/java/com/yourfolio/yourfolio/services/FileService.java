4243df45b420159142bd7b70feadf94dea3ff8df
Bruno López <bruno.lpgz@gmail.com>
2023-05-19 22:16:20 +0200

    Subir archivos funciona

--- yourfolio-backend/yourfolio-service/src/main/java/com/yourfolio/yourfolio/services/FileService.java
+++ yourfolio-backend/yourfolio-service/src/main/java/com/yourfolio/yourfolio/services/FileService.java
@@ -5,6 +5,7 @@ import com.yourfolio.yourfolio.dtos.FileDTO;
 import com.yourfolio.yourfolio.mappers.FileMapper;
 import com.yourfolio.yourfolio.repositories.FileRepository;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +13,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -22,19 +26,46 @@ public class FileService {
     private final FileRepository fileRepository;
     private final FileMapper fileMapper;
 
-    public String getRealPath(String filePath){
+    public Optional<String> getExtension(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".")));
+    }
+
+    /* Recupera la ruta de la carpeta en la que Spring copia los archivos de resources/public.
+    Esta copia se realiza al hacer build. */
+    public String getTargetPath(){
+        return Thread.currentThread().getContextClassLoader().getResource(".").getFile() + "/public/";
+    }
+
+    // Recupera la ruta absoluta de la carpeta resources/build (en caso de no estar la app desplegada)
+    public String getResourcesPath(){
         String resourcesPath = Thread.currentThread().getContextClassLoader().getResource(".").getFile()
-                .replace("target/classes","src/main/resources/public");
-        return resourcesPath.contains("backend") ? resourcesPath : "" + filePath;
+                .replace("target/classes","src/main/resources/public/");
+        return resourcesPath.contains("yourfolio-backend") ? resourcesPath : "";
     }
     public FileDTO uploadImage(MultipartFile image) {
         String uploadDir = env.getProperty("image.upload-dir");
         // Save the image file to a directory on the server
-        String fileName = image.getOriginalFilename();
+        // TODO: comprobar que no existe un archivo igual y que hay extensión
+        String fileName = UUID.randomUUID() + getExtension(image.getOriginalFilename()).get();
+
         String imagePath = uploadDir + fileName;
-        File imageFile = new File(getRealPath(imagePath));
+
+
         try {
+            File imageFile = new File(getResourcesPath() + imagePath);
             image.transferTo(imageFile);
+
+            // Copiamos las imágenes de resource/public a la carpeta target.
+            // Spring lo hace automáticamente, pero solo al hacer build.
+            if (!Objects.equals(getResourcesPath(), ""))
+            {
+                File copied = new File(getTargetPath() + imagePath);
+                FileUtils.copyFile(imageFile, copied);
+            }
+
+
         } catch (IOException e) {
             System.out.println(e);
         }
