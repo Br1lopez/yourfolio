af5f7d20c134dc0ed7fbb0376a4152358f1c5d2b
Bruno López <bruno.lpgz@gmail.com>
2023-05-30 15:45:38 +0200

    new builds

--- yourfolio-backend/yourfolio-boot/src/main/java/com/yourfolio/yourfolio/config/SecurityConfig.java
+++ yourfolio-backend/yourfolio-boot/src/main/java/com/yourfolio/yourfolio/config/SecurityConfig.java
@@ -27,48 +27,57 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-
-
     @Bean
+    // Configuración global de CORS:
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://localhost:3000",
-                "http://localhost:8088",
-                "http://localhost:8088/*",
-                "http://localhost:8088/login",
-                "https://localhost:8088"));
+                "https://localhost:3000"
+             ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-        //source.registerCorsConfiguration("/user/**", configuration);
         return source;
     }
 
     @Bean
+    // Configuración global de seguridad:
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+       // Utilizar la configuración de CORS global:
                 .cors(withDefaults())
                 .csrf().disable()
                 .authorizeHttpRequests()
+
+        // Permitir que cualquier visitante se registre:
                     .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+
+        // Permitir que solo los usuarios autenticados accedan a operaciones que modifiquen la BBDD:
                     .requestMatchers(HttpMethod.POST).authenticated()
                     .requestMatchers(HttpMethod.PUT).authenticated()
                     .requestMatchers(HttpMethod.PATCH).authenticated()
                     .requestMatchers(HttpMethod.DELETE).authenticated()
+
+        // Permitir que cualquier visitante pueda ver el contenido público:
                     .requestMatchers(HttpMethod.GET).permitAll()
                     .anyRequest().permitAll()
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                     .and()
+
+        // Creación del endpoint /login para iniciar sesión:
                 .formLogin()
                     .usernameParameter("email")
+
+        /*
+        Uso de las clases personalizadas LoginSuccesHandler() y LoginFailureHandler()
+        para gestionar la autenticación:
+        */
                     .successHandler(new LoginSuccesHandler())
                     .failureHandler(new LoginFailureHandler());
         return http.build();
--- yourfolio-backend/yourfolio-repository/src/main/java/com/yourfolio/yourfolio/repositories/ElementRelationshipRepository.java
+++ yourfolio-backend/yourfolio-repository/src/main/java/com/yourfolio/yourfolio/repositories/ElementRelationshipRepository.java
@@ -10,15 +10,20 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ElementRelationshipRepository extends JpaRepository<ElementRelationshipEntity, ElementRelationshipEntityId> {
+
+    /* JPA ofrece métodos por defecto para operaciones básicas como guardar o leer,
+    pero permite que se le agreguen funcionalidades de diversas formas: */
+
+    // Aquí JPA crea la query personalizada a partir del nombre
+    List<ElementRelationshipEntity> findByParentIdOrderByPositionAsc(Integer parentId);
+
+    // Aquí se le ha indicado la query a mano
+    @Query(value = "SELECT MAX(position) FROM element_children WHERE parent_id= :parentId", nativeQuery = true)
+    Integer findMaxPosition(@Param("parentId") Integer parentId);
+
     ElementRelationshipEntity findByParentIdAndChildId(Integer parentId, Integer childId);
 
     List<ElementRelationshipEntity> findByChildId(Integer childId);
-
-    List<ElementRelationshipEntity> findByParentIdOrderByPositionAsc(Integer parentId);
-
-
-    @Query(value = "SELECT MAX(position) FROM element_children WHERE parent_id= :parentId", nativeQuery = true)
-    Integer findMaxPosition(@Param("parentId") Integer parentId);
 
     long countByParentId(Integer parentId);
 
